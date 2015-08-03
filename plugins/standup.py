dc4a5edbdacb1c9a717f42eb96a90ee019e0923c
Rene Dohmen <acidjunk@gmail.com>
2015-08-03 21:33:47 +0200

    Added StandUp reminder

---
+++ plugins/standup.py
@@ -0,0 +1,8 @@
+from will.plugin import WillPlugin
+from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
+
+class StandupPlugin(WillPlugin):
+
+    @periodic(hour='9', minute='30', day_of_week="mon-fri")
+    def standup(self):
+        self.say("@all Standup! %s" % settings.WILL_HANGOUT_URL)
